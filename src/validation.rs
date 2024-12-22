84f573847355689c54f9fb2f2b7a56a2babae32b
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-12-22 14:08:34 +0530

    added comment

--- src/validation.rs
+++ src/validation.rs
@@ -1,13 +1,17 @@
 use sqlx::{Pool, Postgres};
 
 pub async fn check_emails(emails: &[String], connection: Pool<Postgres>) -> Result<bool, String> {
+    // Checks if all emails are present in the database. 
+    // Relies on the UNIQUE constraint on email field in user_profile table
+
     let count = emails.len();
     let count_in_db = sqlx::query_scalar!(
         "SELECT COUNT(*) AS \"count!\" FROM user_profile WHERE email = ANY($1)",
         emails
     )
     .fetch_one(&connection)
-    .await.map_err(|e| e.to_string())?;
+    .await
+    .map_err(|e| e.to_string())?;
 
     Ok(count_in_db == count as i64)
 }
